57957ef9fa9933ad85f8f02695a204216568a0f8
Scott Frederick <scottyfred@gmail.com>
2018-10-12 16:31:49 -0500

    Create new client.

--- src/main/java/com/example/test/TestApplication.java
+++ src/main/java/com/example/test/TestApplication.java
@@ -3,7 +3,14 @@ package com.example.test;
 import org.cloudfoundry.client.CloudFoundryClient;
 import org.cloudfoundry.client.v2.organizations.ListOrganizationsRequest;
 import org.cloudfoundry.operations.CloudFoundryOperations;
+import org.cloudfoundry.operations.DefaultCloudFoundryOperations;
 import org.cloudfoundry.operations.organizations.OrganizationSummary;
+import org.cloudfoundry.reactor.client.ReactorCloudFoundryClient;
+import org.cloudfoundry.reactor.tokenprovider.ClientCredentialsGrantTokenProvider;
+import org.cloudfoundry.uaa.UaaClient;
+import org.cloudfoundry.uaa.clients.CreateClientRequest;
+import org.cloudfoundry.uaa.clients.DeleteClientRequest;
+import org.cloudfoundry.uaa.tokens.GrantType;
 import org.cloudfoundry.util.ResourceUtils;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -17,11 +24,14 @@ public class TestApplication implements CommandLineRunner {
 
 	private CloudFoundryOperations cloudFoundryOperations;
 	private CloudFoundryClient cloudFoundryClient;
+	private UaaClient uaaClient;
 
 	public TestApplication(final CloudFoundryOperations cloudFoundryOperations,
-						   final CloudFoundryClient cloudFoundryClient) {
+						   final CloudFoundryClient cloudFoundryClient,
+						   final UaaClient uaaClient) {
 		this.cloudFoundryOperations = cloudFoundryOperations;
 		this.cloudFoundryClient = cloudFoundryClient;
+		this.uaaClient = uaaClient;
 	}
 
 	public static void main(String[] args) {
@@ -30,9 +40,29 @@ public class TestApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) {
+		this.uaaClient.clients().delete(DeleteClientRequest.builder()
+				.clientId("test-client")
+				.build())
+				.then(this.uaaClient.clients().create(CreateClientRequest.builder()
+						.clientId("test-client")
+						.clientSecret("test-client-secret")
+						.authorizedGrantTypes(GrantType.CLIENT_CREDENTIALS)
+						.authorities("uaa.resource")
+						.scopes("cloud_controller.read", "cloud_controller.write", "cloud_controller.admin", "openid")
+						.build()))
+				.block();
+
+		ReactorCloudFoundryClient newClient = ReactorCloudFoundryClient.builder()
+				.from((ReactorCloudFoundryClient) cloudFoundryClient)
+				.tokenProvider(ClientCredentialsGrantTokenProvider.builder()
+						.clientId("test-client")
+						.clientSecret("test-client-secret")
+						.build())
+				.build();
+
 		log.info("Listing organizations using client");
-		
-		this.cloudFoundryClient.organizations().list(ListOrganizationsRequest.builder()
+
+		newClient.organizations().list(ListOrganizationsRequest.builder()
 				.build())
 				.flatMapMany(ResourceUtils::getResources)
 				.map(ResourceUtils::getId)
@@ -41,9 +71,14 @@ public class TestApplication implements CommandLineRunner {
 
 		log.info("Finished listing organizations using client");
 
+		CloudFoundryOperations newOperations = DefaultCloudFoundryOperations.builder()
+				.from((DefaultCloudFoundryOperations) cloudFoundryOperations)
+				.cloudFoundryClient(newClient)
+				.build();
+
 		log.info("Listing organizations using operations");
 
-		this.cloudFoundryOperations.organizations()
+		newOperations.organizations()
 				.list()
 				.map(OrganizationSummary::getName)
 				.doOnNext(orgName -> log.info("found organization '{}'", orgName))
